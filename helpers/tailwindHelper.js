d093b798f1853fb9ad0f5344bcfb65ceb6ed11aa
OliverPanda <380425169@qq.com>
2021-09-07 11:51:36 +0800

    feat: tailwind引入 + commit提交规范
    css解决方案选型为CSS in JS, 选择tailwind + antd色彩方案;

---
+++ helpers/tailwindHelper.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // antd colors是个数组, tailwind是对象 转换函数
+  antdColor2Tailwind () {
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header m-4 flex">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
       </header>
     </div>
   );
