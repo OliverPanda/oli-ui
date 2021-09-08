2aba08d5bd2da775acc2139dd61e9ec6b9e5a553
OliverPanda <380425169@qq.com>
2021-09-08 17:49:24 +0800

    fix: 删除tailwind helper

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("F:\\练手项目\\React项目\\oli-ui\\.docz\\.cache\\dev-404-page.js"))),
+  "component---readme-md": (preferDefault(require("F:\\练手项目\\React项目\\oli-ui\\README.md"))),
+  "component---src-components-button-button-mdx": (preferDefault(require("F:\\练手项目\\React项目\\oli-ui\\src\\components\\Button\\button.mdx"))),
+  "component---src-pages-404-js": (preferDefault(require("F:\\练手项目\\React项目\\oli-ui\\.docz\\src\\pages\\404.js")))
+}
+
--- helpers/tailwindHelper.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // antd colors是个数组, tailwind是对象 转换函数
-  antdColor2Tailwind () {
-  }
-}
